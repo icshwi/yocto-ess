74e997ca13494d585070447a2f39ce1cd22b9b19
Oliver Talevski <oliver.talevski@sigmaconnectivity.se>
2019-04-18 07:59:26 +0200

    Remove domain from hostname if it is set

--- sources/meta-ess/recipes-ess/services/files/host-init.sh
+++ sources/meta-ess/recipes-ess/services/files/host-init.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 BOOTSERVER=$(cat /proc/net/pnp | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+HOST=$(echo $HOSTNAME | grep -o '^[^.]\+')
 
-tftp -g -r boot/scripts/setup-$HOSTNAME.sh -l /usr/share/host-init/setup-$HOSTNAME.sh $BOOTSERVER
+tftp -g -r boot/scripts/setup-$HOST.sh -l /usr/share/host-init/setup-$HOST.sh $BOOTSERVER
 
-if [ -e "/usr/share/host-init/setup-$HOSTNAME.sh" ]; then
-	chmod +x /usr/share/host-init/setup-$HOSTNAME.sh
-	/usr/share/host-init/setup-$HOSTNAME.sh
+if [ -e "/usr/share/host-init/setup-$HOST.sh" ]; then
+	chmod +x /usr/share/host-init/setup-$HOST.sh
+	/usr/share/host-init/setup-$HOST.sh
 else
-	echo "File /usr/share/host-init/setup-$HOSTNAME.sh not found!" > /dev/kmsg
-	echo "Hostname: $HOSTNAME, Bootserver: $BOOTSERVER" > /dev/kmsg
+	echo "File /usr/share/host-init/setup-$HOST.sh not found!" > /dev/kmsg
+	echo "Hostname: $HOST, Bootserver: $BOOTSERVER" > /dev/kmsg
 fi
 

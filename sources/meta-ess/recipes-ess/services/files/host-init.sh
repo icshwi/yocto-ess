be18e491d98f6ef116027881d7d0141525aff727
Ross Elliot <ross.elliot@esss.se>
2019-06-20 10:12:49 +0200

    Check existence of csi user before creation

--- sources/meta-ess/recipes-ess/services/files/host-init.sh
+++ sources/meta-ess/recipes-ess/services/files/host-init.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 
+# Check for existence of csi user ...
+# Run "id -u" command for csi user, and redirect output + errors to stdout
+id -u csi >/dev/null 2>&1
+
+# Check exit code for command { 0 == User exists, 1 == User doesn't exist }
+if ! [ $? -eq 0 ]; then
+
+# User not found, now create user ...
+
 # Create csi user
 adduser csi
 echo 'csi:$6$rounds=656000$dU2LteK8bw34CcUd$UrTE09vKKtQTF6sj8OSrRVzs5uzLlPAgpmsAK3kzSUztiSesIauzB5nHgiG93EdwGNsa4h/jQ6Yn21V28Bs7s0' | chpasswd -e
 echo "csi ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/csi
 mkdir /home/csi/.ssh
+
 cat << EOF > /home/csi/.ssh/authorized_keys
 ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAF/XCunlPgvj8LdgcFP2iJ8pe/oPfs68ANxYLoHfgNh8xzBkeqaIDfQIhzT1nlZDeaMwqEXI5Q85w2HtOKZSnaDGAHs4SCV1iFtvZyUnOoevAGHF5uyOmjyxveHAR/psvMxVBE/O403PsVdcfpQfVGEwV7se8bCa1S7YiQtN/bLigpgwg== csi-deploy-key
 EOF
 chown -R csi:csi /home/csi/.ssh
 chmod 600 /home/csi/.ssh/authorized_keys
 
-
 # Disable password auth. SSH logon is via ssh key only.
 echo "Disabling password auth in sshd_config"
 #sed s/PasswordAuthentication\ yes/PasswordAuthentication\ no/ -i /etc/ssh/sshd_config
 
 echo -e "\nMatch User csi\n    PasswordAuthentication no" >> /etc/ssh/sshd_config
+
+fi
 # End csi user
 
 
